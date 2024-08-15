acc3d86ddf539962139151167c053b12bc971d10
chien67 <95527965+chien67@users.noreply.github.com>
2024-08-15 15:12:09 +0700

    Edit std

--- src/MVC_Student/Models/Student.cs
+++ src/MVC_Student/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,15 @@ namespace MVC_Student.Models
     public class Student
     {
         public int StudentId { get; set; }
+        
+        [Display(Name = "name")]
         public string StudentName { get; set; }
         public int Age { get; set; }
+        public Standard standard { get; set; }
+    }
+    public class Standard
+    {
+        public int SrandardID { get; set; }
+        public string StandardName { get; set; }
     }
 }
